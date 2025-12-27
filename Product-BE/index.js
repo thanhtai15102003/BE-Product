e547ab69e70d696a6fecf2b56703f796c72a0825
thanhtai15102003 <thainguyenthanhtai2003@gmail.com>
2025-12-27 10:07:55 +0700

    Xay dung Layout chung

--- Product-BE/index.js
+++ Product-BE/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 require('dotenv').config();
 
-const database = require("./config/database")
+const database = require("./config/database");
+const systemConfig = require('./config/system')
 
 const route = require('./routes/client/index.route');
 const routeAdmin = require('./routes/admin/index.route');
@@ -17,6 +18,9 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.use(express.static('public'));
+
+//App locals Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 route(app);
 routeAdmin(app);
