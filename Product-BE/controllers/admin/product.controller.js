313d8e30101cd241a3609a7b10bd46b78433c48b
thanhtai15102003 <thainguyenthanhtai2003@gmail.com>
2025-12-27 23:10:39 +0700

    Lam tinh nang bo loc trang thai page admin

--- Product-BE/controllers/admin/product.controller.js
+++ Product-BE/controllers/admin/product.controller.js
@@ -1,14 +1,45 @@
-const Product = require('../../models/product.model')
+const Product = require('../../models/product.model');
 
 // [GET] /admin/products
 
-module.exports.index =async (req, res) => {
-    const products = await Product.find({
+module.exports.index = async (req, res) => {
+    let filterStatus = [
+        {
+            name: 'Tất cả',
+            status: '',
+            class: ''
+        },
+        {
+            name: 'Hoạt động',
+            status: 'active',
+            class: ''
+        },
+        {
+            name: 'Ngưng hoạt động',
+            status: 'inactive',
+            class: ''
+        }
+    ];
+
+    if (req.query.status) {
+        const index = filterStatus.findIndex((item) => item.status === req.query.status);
+        filterStatus[index].class = 'active';
+    } else {
+        filterStatus[0].class = 'active';
+    }
+    // console.log(req.query.status);
+    let find = {
         deleted: false
-    });
-    
+    };
+    if (req.query.status) {
+        find.status = req.query.status;
+    }
+
+    const products = await Product.find(find);
+
     res.render('admin/pages/products/index', {
         pageTitle: 'Danh sách sản phẩm',
-        products: products
+        products: products,
+        filterStatus: filterStatus
     });
 };
