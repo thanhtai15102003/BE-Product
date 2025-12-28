16b3d3179a9d7eaa58faa403e73f5637bce04417
thanhtai15102003 <thainguyenthanhtai2003@gmail.com>
2025-12-28 22:31:02 +0700

    Toi uu phan bo loc

--- Product-BE/controllers/admin/product.controller.js
+++ Product-BE/controllers/admin/product.controller.js
@@ -1,47 +1,23 @@
 const Product = require('../../models/product.model');
+const filterStatusHelper = require('../../helpers/filterStatus');
 
 // [GET] /admin/products
 
 module.exports.index = async (req, res) => {
-    let filterStatus = [
-        {
-            name: 'Tất cả',
-            status: '',
-            class: ''
-        },
-        {
-            name: 'Hoạt động',
-            status: 'active',
-            class: ''
-        },
-        {
-            name: 'Ngưng hoạt động',
-            status: 'inactive',
-            class: ''
-        }
-    ];
-
-    if (req.query.status) {
-        const index = filterStatus.findIndex((item) => item.status === req.query.status);
-        filterStatus[index].class = 'active';
-    } else {
-        filterStatus[0].class = 'active';
-    }
-    // console.log(req.query.status);
+    const filterStatus = filterStatusHelper(req.query);
     let find = {
-        deleted: false,
+        deleted: false
     };
-    let keyword = "";
-    if (req.query.keyword) {
-        keyword = req.query.keyword;
-
-        const regex = new RegExp(keyword, "i");
-        find.title = regex;
-    }
     if (req.query.status) {
         find.status = req.query.status;
     }
+    let keyword = '';
+    if (req.query.keyword) {
+        keyword = req.query.keyword;
 
+        const regex = new RegExp(keyword, 'i');
+        find.title = regex;
+    }
     const products = await Product.find(find);
 
     res.render('admin/pages/products/index', {
