8525352624f70b1ab3b12d85fa365eb8757fa94b
thanhtai15102003 <thainguyenthanhtai2003@gmail.com>
2025-12-28 16:36:22 +0700

    Tinh nag tim kiem Client

--- Product-BE/public/admin/js/script.js
+++ Product-BE/public/admin/js/script.js
@@ -1,20 +1,36 @@
 //Button status
-const buttonStatus = document.querySelectorAll("[button-status]")
+const buttonStatus = document.querySelectorAll('[button-status]');
 if (buttonStatus.length > 0) {
     let url = new URL(window.location.href);
-    buttonStatus.forEach(button => {
-        button.addEventListener("click", () => {
-            const status = button.getAttribute("button-status")
+    buttonStatus.forEach((button) => {
+        button.addEventListener('click', () => {
+            const status = button.getAttribute('button-status');
             if (status) {
-                url.searchParams.set("status", status)   
-            }
-            else {
-                 url.searchParams.delete('status');  
+                url.searchParams.set('status', status);
+            } else {
+                url.searchParams.delete('status');
             }
             console.log(url.href);
             window.location.href = url.href;
-        })
-    })
+        });
+    });
 }
-console.log(buttonStatus)
 // end Button status
+
+// Chức năng tìm kiếm sản phẩm Admin
+const formSearch = document.querySelector('#form-search');
+if (formSearch) {
+    let url = new URL(window.location.href);
+    formSearch.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const keyword = e.target.elements.keyword.value;
+        console.log(e.target.elements.keyword.value);
+        if (keyword) {
+            url.searchParams.set('keyword', keyword);
+        } else {
+            url.searchParams.delete('keyword');
+        }
+        window.location.href = url.href;
+    });
+}
+// End Chức năng tìm kiếm sản phẩm Admin
