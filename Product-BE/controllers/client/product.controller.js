68a1cdcdfcbae9feeb2e7e8404dc9bdcc6d9826a
thanhtai15102003 <thainguyenthanhtai2003@gmail.com>
2026-01-13 09:39:08 +0700

    Tinh nang chi tiet san pham ben client

--- Product-BE/controllers/client/product.controller.js
+++ Product-BE/controllers/client/product.controller.js
@@ -1,3 +1,4 @@
+const slugGenerator = require('mongoose-slug-updater/lib/slug-generator');
 const Product = require('../../models/product.model');
 
 // [GET] /products
@@ -10,10 +11,28 @@ module.exports.index = async (req, res) => {
         item.priceNew = ((item.price * (100 - item.discountPercentage)) / 100).toFixed(0);
         return item;
     });
-    console.log(newProducts);
 
     res.render('client/pages/products/index', {
         pageTitle: 'Danh sách Sản phẩm',
         products: newProducts
     });
 };
+
+// [GET] /products/:slug
+module.exports.detail = async (req, res) => {
+    try {
+            const find = {
+                deleted: false,
+                slug: req.params.slug,
+                status: 'active'
+            };
+            const product = await Product.findOne(find);
+            console.log(product);
+            res.render('client/pages/products/detail', {
+                pageTitle: product.title,
+                product: product
+            });
+        } catch (error) {
+            res.redirect(`/products`);
+        }
+}
